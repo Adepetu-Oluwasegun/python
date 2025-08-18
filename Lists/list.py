3b80bfbc37aefa8f2b039ad38063f328750ce4b7
Oluwasegun Adepetu <Oluwasegunadepetu@gmail.com>
2025-08-18 14:50:39 +0100

    list

---
+++ Lists/list.py
@@ -0,0 +1,16 @@
+# Lists are used to manage, organize and store lists of information
+friends = ["Rajeesh koothrapali", "Ogbono Merchant", "poseidon", "Richmonk", "Basic Bitch"]
+
+print(friends) 
+
+# if i want to access a specific element in my friend list i cwill just use index
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[3])
+print(friends[4])
+print(friends[1:3])
+
+#modifying a particular element
+friends[4] = "sirhme la cash"
+print(friends[4])
