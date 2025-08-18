0006d02218d9630f319cc10a9bec588d363903f6
Oluwasegun Adepetu <Oluwasegunadepetu@gmail.com>
2025-08-18 14:49:37 +0100

    variables

---
+++ Variables/variable.py
@@ -0,0 +1,13 @@
+# # variable assign data to a variable they can hold different values, they are case sensitive and don't start with numbers.
+red_bucket = "kevin"
+print(red_bucket)
+
+# # request input and assign it to a variable
+red_bucket = input("what do you what to put in the bucket? ")
+
+# getting inputs from a user: letting a user input info into our program and store it inside a variable
+name = input("Enter your name: ")
+
+age = input("Enter your age: ")
+
+print("hey " + name + "! you are "  + age)
