26a8cd6b96098039b09d1a7e575b9b401cbd0ca5
Oluwasegun Adepetu <Oluwasegunadepetu@gmail.com>
2025-08-18 14:51:42 +0100

    tuples

---
+++ tuples/tuples.py
@@ -0,0 +1,7 @@
+# its a container we cn store different values. its differnts from lists with some key differences. tuples are immutable(they can't be changed or modified) unlike lists. elements in a tuple are easilt accesible
+
+coordinates = (4, 5)
+print(coordinates[1])
+
+# a list of tuples
+coordinates = [(4,5), (7,6), (9,3)]
