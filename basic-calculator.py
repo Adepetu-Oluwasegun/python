d951462434df4df9501a4d597365cb24106b965e
Oluwasegun Adepetu <Oluwasegunadepetu@gmail.com>
2025-08-19 23:25:22 +0100

    basic calculator

---
+++ basic-calculator.py
@@ -0,0 +1,6 @@
+# Building a basic calculator where we get two numbers from a user and we add it together and print it to our screen
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result =int(num1) + int(num2)
+
+result = float(num1) + float(num2)
