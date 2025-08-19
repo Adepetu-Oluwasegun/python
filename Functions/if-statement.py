cc48a76a614587e72e8e62544852e5135b9be9e1
Oluwasegun Adepetu <Oluwasegunadepetu@gmail.com>
2025-08-19 23:24:56 +0100

    if statement

---
+++ Functions/if-statement.py
@@ -0,0 +1,20 @@
+#
+is_male = True
+
+if is_male:
+  print("You are a man")
+else:
+  print("you are not a man")
+
+is_female = False
+
+if is_female:
+  print("you are not a woman")
+else:
+  print("you are a woman")
+
+is_tall = True
+is_playful = True
+
+if is_tall or is_playful:
+  print("he is tall and playful")
