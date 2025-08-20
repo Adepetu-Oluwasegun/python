c633feb01cfc22249c4239fbd15e85bf9b4eeeaf
Oluwasegun Adepetu <Oluwasegunadepetu@gmail.com>
2025-08-20 11:16:15 +0100

    comparison-operators

---
+++ Operators/comparisions-operators.py
@@ -0,0 +1,18 @@
+def max_num(num1, num2, num3):
+  if num1 >= num2 and num1 >= num3:
+    return num1
+  elif num2 >= num1 and num2 >= num3:
+    return num2
+  else:
+    return num3
+
+print(max_num(300,40,5))
+print(max_num(29,25,50))
+
+def min_num(num1, num2, num3):
+  if num1 <= num2 and num1 >= num3:
+    return num1
+  elif num2 >= num1 and num2 <= num3:
+    return num2
+  else:
+    return num3
