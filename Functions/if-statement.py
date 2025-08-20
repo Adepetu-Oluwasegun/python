990ebeb293525fcff6b17b6c4bcebf3338a010a8
Oluwasegun Adepetu <Oluwasegunadepetu@gmail.com>
2025-08-20 11:16:45 +0100

    update

--- Functions/if-statement.py
+++ Functions/if-statement.py
@@ -18,3 +18,18 @@ is_playful = True
 
 if is_tall or is_playful:
   print("he is tall and playful")
+
+is_male = True
+is_tall = False
+
+if is_male and is_tall:
+  print("you are a tall man")
+  
+elif is_male and not(is_tall):
+  print("you are a short man")
+
+elif not is_male and (is_tall):
+  print("you are not a man but you are tall")
+
+else:
+  print("you are either a man or tall")
