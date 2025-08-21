9e5a5452b2e652dbf4d395321ec550726f7351d6
Oluwasegun Adepetu <Oluwasegunadepetu@gmail.com>
2025-08-21 17:52:01 +0100

    update

--- basic-calculator.py
+++ basic-calculator.py
@@ -1,6 +1,23 @@
 # Building a basic calculator where we get two numbers from a user and we add it together and print it to our screen
-num1 = input("Enter a number: ")
-num2 = input("Enter another number: ")
-result =int(num1) + int(num2)
+# num1 = input("Enter a number: ")
+# num2 = input("Enter another number: ")
+# result =int(num1) + int(num2)
 
-result = float(num1) + float(num2)
+
+#  building advance calculator
+
+nom1 = float(input ("Enter the first number: "))
+op = input("Enter operator: ")
+
+nom2 = float(input ("Enter the second number: "))
+
+if op == "+":
+  print(nom1 + nom2)
+elif op == "-":
+  print(nom1 - nom2)
+elif op == "/":
+  print(nom1 / nom2)
+elif op == "*":
+  print(nom1 * nom2)
+else:
+  print("invalid operator")
