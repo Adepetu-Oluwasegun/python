6fd4fdd0c0ddc91a4a185c25a74033fab17ae5d8
Oluwasegun Adepetu <Oluwasegunadepetu@gmail.com>
2025-09-01 12:40:42 +0100

    basic translator

---
+++ basic_translator.py
@@ -0,0 +1,17 @@
+# building a basic translator that can take in a string or phrase and translate it into a draft language
+def translate(phrase):
+  translation = ""
+
+  for letter in phrase:
+    if letter.lower() in "aeiou":
+     if letter.upper():
+      translation = translation + "G"
+
+    else:
+      translation = translation + "g"
+
+  else:
+      translation = translation + letter
+  return translation
+  
+print(translate(input("Enter a phrase: ")))
