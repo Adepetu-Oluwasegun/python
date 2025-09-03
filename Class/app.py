180976edebc323e7eddf1ec0cc079a380c678163
Oluwasegun Adepetu <Oluwasegunadepetu@gmail.com>
2025-09-03 15:06:59 +0100

    update

---
+++ Class/app.py
@@ -0,0 +1,8 @@
+from student import Student
+
+student1 = Student("fola", "computer science", 3.5, False) #object student is the instance of class student
+
+print(student1.gpa)
+print(student1.major)
+print(student1.name)
+print(student1.is_on_probation)
