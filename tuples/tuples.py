23bde0b2e707cc62aca9fab5c146b8745e70f3b1
Oluwasegun Adepetu <Oluwasegunadepetu@gmail.com>
2025-09-15 14:00:32 +0100

    modified file

--- tuples/tuples.py
+++ tuples/tuples.py
@@ -1,4 +1,4 @@
-# its a container we cn store different values. its differnts from lists with some key differences. tuples are immutable(they can't be changed or modified) unlike lists. elements in a tuple are easilt accesible
+# its a container we can store different values. its different from lists. with some key differences, tuples are immutable(they can't be changed or modified) unlike lists. elements in a tuple are easilt accesible
 
 coordinates = (4, 5)
 print(coordinates[1])
