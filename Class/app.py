039339817d4da42f7f474274dec27d3e6fcaca9a
Oluwasegun Adepetu <Oluwasegunadepetu@gmail.com>
2025-09-18 09:23:51 +0100

    update

--- Class/app.py
+++ Class/app.py
@@ -1,8 +1,12 @@
 from student import Student
 
-student1 = Student("fola", "computer science", 3.5, False) #object student is the instance of class student
+student1 = Student("fola", "computer science", 3.5, False, 4) #object student is the instance of class student
+student2 = Student("shola", "chemistry", 1.5, True, 6)
 
 print(student1.gpa)
-print(student1.major)
+print(student1.course)
 print(student1.name)
 print(student1.is_on_probation)
+print(student1.year_of_study)
+print(student1.on_hon_roll())
+print(student2.on_hon_roll())
